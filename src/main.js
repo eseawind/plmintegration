e86a81e5f3ad8c57cf63c899b0d6587defa9843b
Jonas Colmsjö <jonas.colmsjo@gizur.com>
2014-12-10 08:55:23 +0100

    Parsed the example files

--- src/main.js
+++ src/main.js
@@ -1,6 +1,47 @@
-var parseString = require('xml2js').parseString;
+var fs = require('fs');
+var XmlStream = require('xml-stream');
 
-var xml = "<root>Hello xml2js!</root>"
-parseString(xml, function (err, result) {
-  console.dir(result);
-});
+
+//
+// Parsing the file filePath, look for xmlElements and return a JSON structure
+// ---------------------------------------------------------------------------
+
+
+var p = function(filePath, xmlElements, cb) {
+
+  var inStream = fs.createReadStream(filePath);
+  var xml = new XmlStream(inStream);
+
+  var elements = {};
+
+  xmlElements.forEach(function(e) {
+
+    elements[e] = [];
+
+    xml.preserve(e, true);
+    xml.on('endElement: '+e, function(element) {
+      elements[e].push(element);
+    });
+
+    xml.on('end', function () {
+      console.log('Found %d %s elements in %s.', elements[e].length, e, filePath);
+      if (cb !== undefined) cb(elements);
+    });
+
+  });
+
+  xml.on('error', function (err) {
+    console.log('error: '+err);
+  });
+
+};
+
+
+//
+// Main
+// ----
+
+p('./examples/ESIResponse_AddedComponent.xml', ['Part']);
+p('./examples/ESIResponse_InitialExport.xml', ['Part','BOMHeader']);
+p('./examples/ESIResponse_RemovedComponent.xml', ['ChangedParts','ChangedBOMs', 'UnchangedParts', 'AddedBOMs']);
+p('./examples/ESIResponse_UpdatedLink.xml', ['ChangedParts','ChangedBOMs', 'UnchangedParts', 'AddedBOMs']);
